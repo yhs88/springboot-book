9c0ca4eb922766108a312cf1af73c7bfb801c662
vicotr <victor@within.co.kr>
2019-03-13 00:53:32 +0900

    oauth2 구현

--- src/main/java/com/web/WebApplication.java
+++ src/main/java/com/web/WebApplication.java
@@ -5,19 +5,32 @@ import com.web.domain.User;
 import com.web.domain.enums.BoardType;
 import com.web.repository.BoardRepository;
 import com.web.repository.UserRepository;
+import com.web.resolver.UserArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
-public class WebApplication {
+public class WebApplication implements WebMvcConfigurer {
+
+    @Autowired
+    private UserArgumentResolver userArgumentResolver;
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(userArgumentResolver);
     }
 
     @Bean
